e844aedda1f7c87f03b43b0b833d743b375af5ef
Lavesh06 <kumarlavesh2001@gmail.com>
2026-03-31 16:37:42 +0530

    banner

--- components/common/homepage/Banner.tsx
+++ components/common/homepage/Banner.tsx
@@ -114,8 +114,8 @@ const Banner = () => {
           ))}
         </CarouselContent>
 
-        <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 z-10" />
-        <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-10" />
+        <CarouselPrevious className="absolute left-4 top-1/2 z-10" />
+        <CarouselNext className="absolute right-4 top-1/2  z-10" />
       </Carousel>
 
       {/* DOTS */}
